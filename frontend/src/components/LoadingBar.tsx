1c3f39fb0f7ac426ef18321e62c4c97a10b62f77
Pooya Alavian <pooyaalavian@microsoft.com>
2024-03-07 04:04:00 -0500

    loading options

---
+++ frontend/src/components/LoadingBar.tsx
@@ -0,0 +1,7 @@
+import './LoadingBar.css';
+
+export const LoadingBar = () => {
+    return <div className="parent bg-gray-300 h-1 w-full">
+        <div className="child bg-blue-600 h-1"></div>
+    </div>;
+};
